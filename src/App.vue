6262f23371d53611d88d016aa3a8c2c58d810745
marcogonca96 <marcogonca1996@gmail.com>
2018-02-13 23:17:12 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+   
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>test</h1>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Footer from './Footer.vue'
+export default {
+    components: {
+        'app-footer':Footer
+    }
+}
+</script>
