10a8464ad20ed8197b869dcad030dfd6cd9165fd
marcogonca96 <marcogonca1996@gmail.com>
2018-02-23 12:07:49 +0000

    something

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
 import Header from './Header.vue'
+import 'vuetify/dist/vuetify.min.css' 
 
 Vue.component('app-header',Header)
 Vue.use(Vuetify)
